f2f6d70b2c6d935d88230ca8bd0d976a9302c143
jeevabharathyit <30828515+jeevabharathyit@users.noreply.github.com>
2017-09-05 17:01:06 +0530

    Create 27.cpp

---
+++ 27.cpp
@@ -0,0 +1,32 @@
+
+#include<iostream.h>
+#include<conio.h>
+void lcm(int,int);
+void main()
+{
+    int a,b;
+    clrscr();
+    cout<<"Enter two numbers: ";
+    cin>>a;
+    cin>>b;
+    lcm(a,b);
+    getch();
+}
+void lcm(int a,int b)
+{
+    int m,n;
+    m=a;
+    n=b;
+    while(m!=n)
+    {
+	if(m < n)
+	{
+	m=m+a;
+	}
+	else
+	{
+	    n=n+b;
+	    }
+    }
+    cout<<"\nL.C.M of "<<a<<" and "<<b<<" is "<<m;
+}
