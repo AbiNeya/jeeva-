f21ba06dd63756858da09ae64c795521af342f6a
jeevabharathyit <30828515+jeevabharathyit@users.noreply.github.com>
2017-09-10 02:51:27 -0700

    Update 44.c

--- 44.c
+++ 44.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
-int main(void) {
-         int input;    
-    	 scanf("%d",&input);
-	       if(input<=9&&input>=1)
-	             printf("entered value with in the range ,no error");
-	    	  else
-                    printf("you entered number is not range");
-	
+int main(void)
+{
+      int input;    
+   scanf("%d",&input);
+	if(input<=9&&input>=1)   
+printf("entered value with in the range ,no error");
+	    else
+printf("you entered number is not range");
 	return 0;
 }
