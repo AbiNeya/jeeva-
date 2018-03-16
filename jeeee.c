730f861ff42fbf8648e5d34e352da9e80e2a9cb4
Jeevabharathi R <30828515+jeevabharathyit@users.noreply.github.com>
2018-03-16 15:53:09 +0530

    Create jeeee.c

---
+++ jeeee.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+int main()
+{
+	int l,b,h,jee,volume;
+	scanf("%d %d %d",&l,&b,&h);
+	volume=l*b*h;
+	jee=2*((l*b)+(b*h)+(h*l));
+	printf("volume=%d\n",volume);
+	printf("TSA=%d",tsa);
+	return 0;
+}
