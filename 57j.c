ca3c3903b1e24a411ccd70053015329ca6c1cfaa
Jeevabharathi R <30828515+jeevabharathyit@users.noreply.github.com>
2018-03-26 21:41:36 +0530

    Create 57j.c

---
+++ 57j.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+printf("\n Enter the numbers:");
+scanf("%d %d",&a,&b);
+swap(a,b);
+return 0;
+}
+void swap(int a,int b)
+{
+int temp;
+temp=a;
+a=b;
+b=temp;
+printf("After swapping:%d %d",a,b);
+getch();
+}
